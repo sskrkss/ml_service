a3ce202a588a129fa5c0b5c99789127d386f451b
sskrk <sskrk@unigine.com>
2026-01-19 13:22:24 +0300

    add service

--- app/models/user.py
+++ app/models/user.py
@@ -1,19 +1,18 @@
 from typing import List
 from base_entity import BaseEntity
 from enums import UserRole
-from balance import Balance
 from transaction import Transaction
 from prediction import Prediction
 
 
 class User(BaseEntity):
-    def __init__(self, id: int, email: str, username: str, password_hash: str):
-        super().__init__(id)
+    def __init__(self, email: str, username: str, password_hash: str):
+        super().__init__()
 
         self._email = email
         self._username = username
         self._password_hash = password_hash
-        self._balance = Balance(user_id=id)
+        self._balance: float = 0
         self._roles: List[UserRole] = [UserRole.USER]
         self._transactions: List[Transaction] = []
         self._predictions: List[Prediction] = []
@@ -31,12 +30,12 @@ class User(BaseEntity):
         return self._password_hash
 
     @property
-    def balance(self) -> Balance:
+    def balance(self) -> float:
         return self._balance
 
     @property
-    def roles(self) -> Balance:
-        return self._balance
+    def roles(self) -> List[UserRole]:
+        return self._roles
 
     @property
     def transactions(self) -> List[Transaction]:
@@ -45,3 +44,7 @@ class User(BaseEntity):
     @property
     def predictions(self) -> List[Prediction]:
         return self._predictions
+
+    def add_role(self, role: UserRole) -> None:
+        if role not in self._roles:
+            self._roles.append(role)
