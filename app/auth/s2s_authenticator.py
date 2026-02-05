7f810c0cb5fd13f9511a9b673cca7472adbfac44
sskrk <sskrk@unigine.com>
2026-02-05 17:16:58 +0300

    lesson5 s2s

--- app/auth/s2s_authenticator.py
+++ app/auth/s2s_authenticator.py
@@ -1,5 +1,3 @@
-import logging
-
 from fastapi import Request, HTTPException, status
 from fastapi.security.utils import get_authorization_scheme_param
 from jose import jwt
@@ -9,13 +7,6 @@ from database.config import get_settings
 
 settings = get_settings()
 S2S_SECRET_KEY = settings.S2S_SECRET_KEY
-
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
-logger = logging.getLogger(__name__)
 
 
 class S2SAuthenticator:
@@ -34,16 +25,19 @@ class S2SAuthenticator:
 
         scheme, token = get_authorization_scheme_param(s2s_token)
 
-        # try:
-        jwt.decode(
-            token=token,
-            key=S2S_SECRET_KEY,
-            algorithms=["HS256"],
-            issuer=self.issuer,
-            subject=self.subject
+        try:
+            jwt.decode(
+                token=token,
+                key=S2S_SECRET_KEY,
+                algorithms=["HS256"],
+                issuer=self.issuer,
+                subject=self.subject
             )
-        # except JWTError:
-        #     raise
+        except JWTError:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid token"
+            )
 
 
 auth_ml_worker = S2SAuthenticator(issuer="ml_worker")
